c60546276d58087b0a08a4c2b3bd7896e3f39cbd
Arman <armanbarseghyan83@gmail.com>
2023-11-30 19:15:01 -0800

    guest model and controller functions created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const eventRoutes = require('./event-routes');
 const commentRoutes = require('./comment-routes');
+const guestRoutes = require('./guest-routes')
 
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
 router.use('/comments', commentRoutes);
+router.use('/guests', guestRoutes)
 
 module.exports = router;
