145eb0918710c7cff21cf977c72cf55417d2b720
Arman <armanbarseghyan83@gmail.com>
2023-12-06 17:02:53 -0800

    guest bug fixed

--- controllers/api/guest-routes.js
+++ controllers/api/guest-routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const Guest = require('../../models/Guest');
 
-// Crate a new guest 
+// Crate a new guest
 router.post('/', async (req, res) => {
-
   try {
     const guest = await Guest.create({
       user_id: req.session.currentUser.userId,
@@ -15,12 +14,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-// delete the guest 
+// delete the guest
 router.delete('/', async (req, res) => {
-
   try {
     const guest = await Guest.destroy({
-      where: { user_id: req.body.id },
+      where: { 
+        user_id: req.session.currentUser.userId, 
+        event_id: req.body.event_id
+      },
     });
     res.status(200).json(guest);
   } catch (err) {
